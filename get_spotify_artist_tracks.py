9b7328fe243fd5c4e42ddfdbd41f4b3e29449e59
ian <kickthechair@gmail.com>
2024-08-08 16:45:09 +0100

    spotify call to get artist data. Had to loop each group type as Spotify api not working with multiple group_type params

--- get_spotify_artist_tracks.py
+++ get_spotify_artist_tracks.py
@@ -2,33 +2,36 @@ import requests
 from spotify_auth import spotify_auth
 import pprint
 
-def get_spotify_artist_tracks(artist_id, market):
-    get_albums_endpoint = f"https://api.spotify.com/v1/artists/{artist_id}/albums?market={market}&limit=50"
+
+def artist_albums(artist_id):
+    artist_album_data = spotify_album_data(artist_id, ["album", "single", "appears_on", "compilation"])
+
+    return artist_album_data
+
+
+def spotify_album_data(artist_id, group_type):
     bearer_token = spotify_auth()["access_token"]
     headers = {"Authorization": f"Bearer {bearer_token}"}
+    print(headers)
 
-    artist_albums = []
-    response_json = requests.get(url=get_albums_endpoint, headers=headers).json()
+    album_data = []
 
-    if response_json["next"] is None:
-        return response_json
-    else:
-        artist_albums.append(response_json)
-        while response_json["next"] is not None:
-            print(headers)
-            next_api_endpoint = response_json["next"]
-            response_json = requests.get(url=next_api_endpoint, headers=headers).json()
-            artist_albums.append(response_json)
+    for album_type in group_type:
+        album_type_endpoint = \
+            f"https://api.spotify.com/v1/artists/{artist_id}/albums?&limit=50&include_groups={album_type}&market=GB"
 
-    print(len(artist_albums))
-    return artist_albums
+        response_json = requests.get(url=album_type_endpoint, headers=headers).json()
 
+        if response_json["next"] is None:
+            album_data.append(response_json)
+        else:
+            album_data.append(response_json)
+            while response_json["next"] is not None:
+                next_call = response_json["next"]
+                response_json = requests.get(url=next_call, headers=headers).json()
+                album_data.append(response_json)
 
+    return album_data
 
 aphex_twin_id = "6kBDZFXuLrZgHnvmPu9NsG"
-peter_bruntnell = "6qQpSCDIS4V5Md1sfgaCkh"
-pprint.pp(get_spotify_artist_tracks(aphex_twin_id, "GB"))
-
-# print(get_spotify_artist_tracks(aphex_twin_id, "GB")["next"])
-# print(get_spotify_artist_tracks(aphex_twin_id, "GB").keys())
-# print(get_spotify_artist_tracks(aphex_twin_id, "GB")["total"])
+pprint.pp(artist_albums(aphex_twin_id))
