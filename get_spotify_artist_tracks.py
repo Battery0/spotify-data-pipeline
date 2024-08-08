d050394e4f5e9c9f4e881277b000211ee5d1a983
ian <kickthechair@gmail.com>
2024-08-08 13:34:13 +0100

    working on looping api call to get all artist albums

--- get_spotify_artist_tracks.py
+++ get_spotify_artist_tracks.py
@@ -1,14 +1,34 @@
 import requests
 from spotify_auth import spotify_auth
-
+import pprint
 
 def get_spotify_artist_tracks(artist_id, market):
-    get_albums_endpoint = f"https://api.spotify.com/v1/artists/{artist_id}/albums?market={market}"
-    headers = {"Authorization": f"Bearer {spotify_auth()['access_token']}"}
+    get_albums_endpoint = f"https://api.spotify.com/v1/artists/{artist_id}/albums?market={market}&limit=50"
+    bearer_token = spotify_auth()["access_token"]
+    headers = {"Authorization": f"Bearer {bearer_token}"}
 
+    artist_albums = []
     response_json = requests.get(url=get_albums_endpoint, headers=headers).json()
-    return response_json
+
+    if response_json["next"] is None:
+        return response_json
+    else:
+        artist_albums.append(response_json)
+        while response_json["next"] is not None:
+            print(headers)
+            next_api_endpoint = response_json["next"]
+            response_json = requests.get(url=next_api_endpoint, headers=headers).json()
+            artist_albums.append(response_json)
+
+    print(len(artist_albums))
+    return artist_albums
+
 
 
 aphex_twin_id = "6kBDZFXuLrZgHnvmPu9NsG"
-print(get_spotify_artist_tracks(aphex_twin_id, "GB"))
+peter_bruntnell = "6qQpSCDIS4V5Md1sfgaCkh"
+pprint.pp(get_spotify_artist_tracks(aphex_twin_id, "GB"))
+
+# print(get_spotify_artist_tracks(aphex_twin_id, "GB")["next"])
+# print(get_spotify_artist_tracks(aphex_twin_id, "GB").keys())
+# print(get_spotify_artist_tracks(aphex_twin_id, "GB")["total"])
