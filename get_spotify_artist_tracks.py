fc57f0d87baa8d770b489f43534630ce59feca62
ian <kickthechair@gmail.com>
2024-08-13 19:52:53 +0100

    refactored getting spotify api data methods

--- get_spotify_artist_tracks.py
+++ get_spotify_artist_tracks.py
@@ -2,13 +2,7 @@ import requests
 from spotify_auth import spotify_auth
 
 
-def artist_albums(artist_id):
-    artist_album_data = spotify_album_data(artist_id, ["album", "single", "appears_on", "compilation"])
-
-    return artist_album_data
-
-
-def spotify_album_data(artist_id, group_type):
+def spotify_album_data(artist_id, group_type, market):
     bearer_token = spotify_auth()["access_token"]
     headers = {"Authorization": f"Bearer {bearer_token}"}
 
@@ -16,7 +10,7 @@ def spotify_album_data(artist_id, group_type):
 
     for album_type in group_type:
         album_type_endpoint = \
-            f"https://api.spotify.com/v1/artists/{artist_id}/albums?&limit=50&include_groups={album_type}&market=GB"
+            f"https://api.spotify.com/v1/artists/{artist_id}/albums?&limit=50&include_groups={album_type}&market={market}"
 
         response_json = requests.get(url=album_type_endpoint, headers=headers).json()
 
@@ -29,4 +23,8 @@ def spotify_album_data(artist_id, group_type):
                 response_json = requests.get(url=next_call, headers=headers).json()
                 album_data.append(response_json)
 
+    print(album_data[0]["items"][0]["name"])
     return album_data
+
+
+spotify_album_data("6kBDZFXuLrZgHnvmPu9NsG", ["album"], "GB")
