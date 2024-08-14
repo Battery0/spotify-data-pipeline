f51cbf6b0adc3b0123c4f744bfe990490bea2a5a
ian <kickthechair@gmail.com>
2024-08-14 11:47:38 +0100

    refactored method name

--- data_lake_storage.py
+++ data_lake_storage.py
@@ -1,7 +1,7 @@
 from google.cloud import storage
 
 
-def upload_from_memory_to_data_lake(bucket_name, contents_to_upload, data_type, destination_blob_name):
+def upload_to_data_lake(bucket_name, contents_to_upload, data_type, destination_blob_name):
     storage_client = storage.Client(project="focal-cipher-432312-h8")
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
