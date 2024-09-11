dbfb77cf8a50b59da5e65550ff6133181be74461
ian <kickthechair@gmail.com>
2024-09-11 13:10:04 +0100

    extracting track metadata

--- spotify_track_and_album_metadata.py
+++ spotify_track_and_album_metadata.py
@@ -17,11 +17,43 @@ def spotify_track_and_album_metadata(spotify_auth_json, grouped_track_ids):
     return track_and_album_metadata
 
 
-
-    # extraction of required metadata
-
-
-
 def extract_album_and_track_metadata(album_and_track_metadata):
+    extracted_track_and_album_metadata = []
+
+    for track_subset in album_and_track_metadata:
+        for top_level_metadata in track_subset["tracks"]:
+            track_metadata = extract_track_metadata(top_level_metadata)
+
+            extracted_track_and_album_metadata.append(track_metadata)
+
+    print(extracted_track_and_album_metadata)
+    return extracted_track_and_album_metadata
+
+
+def extract_track_metadata(top_level_metadata):
+    track_metadata = {
+        "track_isrc_number": f"{top_level_metadata['external_ids']['isrc']}",
+        "track_title": f"{top_level_metadata['name']}",
+        "track_artists": f"{extract_track_artists(top_level_metadata)}"
+    }
+
+    return track_metadata
+
+
+def extract_track_artists(top_level_meta_data):
+    track_artists = []
+
+    if len(top_level_meta_data["artists"]) == 1:
+        single_artist = top_level_meta_data["artists"][0]["name"]
+        return single_artist
+    else:
+        for artists in top_level_meta_data["artists"]:
+            track_artists.append(artists["name"])
+
+    multiple_artists = ", ".join(track_artists)
+    return multiple_artists
+
+
+
 
 
